305aa9dea253478d0bd04791109297d731e2323d
Bohdan Mukha <by.muha@gmail.com>
2022-08-23 22:50:16 +0300

    hero movement works

--- js/app.js
+++ js/app.js
@@ -1,51 +1,132 @@
-let highScore = localStorage.getItem('highScore') ?? 0;
-let currentScore = 0;
-const highHolder = document.querySelector('.high');
-const currentHolder = document.querySelector('.current');
-const infoHolder = document.querySelector('.info');
+const env = {
+  fieldMinX: 0,
+  fieldMinY: -10,
+  fieldMaxX: 505,
+  fieldMaxY: 425,
+  tileWidth: 101,
+  tileHeight: 85,
+  tilesCountX: 5,
+  tilesCountY: 6,
+  currentScore: 0,
+  highScoreHolder: document.querySelector('.high'),
+  messageHolder: document.querySelector('.info'),
+  welcomeMessage: "Let's go!",
+  winMessage: 'You win!',
+  loseMessage: 'You lose!',
+  highScore: localStorage.getItem('highScore') ?? 0,
+  currentScoreHolder: document.querySelector('.current'),
+  updateScore: function (message) {
+    this.currentScoreHolder.innerText = `Current score: ${this.currentScore}`;
+    this.highScoreHolder.innerText = `High score: ${this.highScore}`;
+    this.messageHolder.innerText = `${message}`;
+  },
+  incrementScore: function () {
+    this.currentScore += 1;
+    if (this.currentScore > this.highScore) {
+      this.highScore = this.currentScore;
+      localStorage.setItem('highScore', this.highScore);
+    }
+  },
+  resetScore: function () {
+    this.currentScore = 0;
+  },
+};
 
-const getRandomNumber = (min, max) =>
-  Math.floor(Math.random() * (max - min) + min);
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
 const directions = ['left', 'right'];
 
-const getRandomDirection = () =>
-  directions[Math.floor(Math.random() * directions.length)];
+const getRandomDirection = () => directions[Math.floor(Math.random() * directions.length)];
 
-canvas = {
-  width: 505,
-  height: 606,
-};
+class Player {
+  constructor() {
+    this.sprite = 'images/char-boy.png';
+    this.currX = env.tileWidth * 2;
+    this.currY = env.fieldMinY + env.tileHeight * 5;
+    this.resetDelay = 1000;
+  }
+
+  update(newX = this.currX, newY = this.currY) {
+    (this.currX = newX), (this.currY = newY);
+    console.log(`x: ${this.currX}, y:${this.currY}`);
+  }
+
+  reset() {
+    this.update(env.tileWidth * 2, env.tileHeight * 5);
+  }
+
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.currX, this.currY);
+  }
+
+  handleInput(code) {
+    switch (code) {
+      case 'ArrowLeft':
+        if (this.currX - env.tileWidth < env.fieldMinX) return;
+        this.currX -= env.tileWidth;
+        break;
+      case 'ArrowRight':
+        if (this.currX + env.tileWidth >= env.fieldMaxX) return;
+        this.currX += env.tileWidth;
+        break;
+      case 'ArrowUp':
+        if (this.currY - env.tileHeight < env.fieldMinY) return;
+        this.currY -= env.tileHeight;
+        break;
+      case 'ArrowDown':
+        if (this.currY + env.tileHeight >= env.fieldMaxY) return;
+        this.currY += env.tileHeight;
+        break;
+      default:
+        break;
+    }
+    this.update(this.currX, this.currY);
+    if (this.currY === -6) {
+      env.incrementScore();
+      env.updateScore(env.winMessage);
+      setTimeout(() => {
+        player.update(this.startX, this.startY);
+        env.updateScore(env.welcomeMessage);
+      }, this.resetDelay);
+    }
+  }
+}
+
+const player = new Player();
 
 class Enemy {
-  constructor(startY, mult) {
-    this.width = 101;
-    this.height = 171;
+  constructor(startY, mult, player) {
     this.direction = getRandomDirection();
     this.sprite = `images/enemy-bug-${this.direction}.png`;
-    this.minX = -this.width;
-    this.maxX = canvas.width + this.width;
+    this.minX = -env.tileWidth;
+    this.maxX = env.width + env.tileWidth;
     this.startX = this.direction === 'right' ? this.minX : this.maxX;
     this.startY = startY;
     this.currX = this.startX;
     this.currY = this.startY;
     this.mult = this.direction === 'right' ? mult : -mult;
+    this.player = player;
+  }
+  checkCollision() {
+    if (
+      this.player.currX >= this.currX &&
+      this.player.currX <= this.currX + env.tileWidth &&
+      this.player.currY >= this.currY &&
+      this.player.currY <= this.currY + 85
+    ) {
+      return true;
+    }
+    return false;
   }
   update(dt) {
-    if (
-      player.currX >= this.currX &&
-      player.currX <= this.currX + this.width &&
-      player.currY >= this.currY &&
-      player.currY <= this.currY + 85
-    ) {
-      infoHolder.innerText = `You lost`;
-      currentScore = 0;
-      currentHolder.innerText = `Current score: ${currentScore}`;
+    if (this.checkCollision()) {
+      env.resetScore();
+      env.updateScore(env.loseMessage);
       setTimeout(() => {
-        player.update(player.startX, player.startY);
-        infoHolder.innerText = `Let's go!`;
+        this.player.updateScore(player.startX, player.startY);
+        env.updateScore(env.welcomeMessage);
       }, 1000);
-      player.update(player.startX, player.startY);
+      this.player.updateScore(player.startX, player.startY);
     }
     if (this.currX >= this.maxX) {
       this.mult *= -1;
@@ -63,80 +144,11 @@ class Enemy {
   }
 }
 
-class Player {
-  constructor() {
-    this.sprite = 'images/char-boy.png';
-    this.width = 101;
-    this.height = 171;
-    this.startX = 203;
-    this.startY = 404;
-    this.currX = this.startX;
-    this.currY = this.startY;
-    this.minX = 0;
-    this.maxX = canvas.width;
-    this.stepX = this.width;
-    this.minY = -50;
-    this.maxY = 450;
-    this.stepY = 82;
-  }
-
-  update(newX = this.currX, newY = this.currY) {
-    (this.currX = newX), (this.currY = newY);
-  }
-
-  render() {
-    ctx.drawImage(Resources.get(this.sprite), this.currX, this.currY);
-  }
-
-  getHighcore() {
-    return localStorage.getItem('highScore') || 0;
-  }
-
-  handleInput(code) {
-    switch (code) {
-      case 'ArrowLeft':
-        if (this.currX - this.stepX <= this.minX) return;
-        this.currX -= this.stepX;
-        break;
-      case 'ArrowRight':
-        if (this.currX + this.stepX >= this.maxX) return;
-        this.currX += this.stepX;
-        break;
-      case 'ArrowUp':
-        if (this.currY - this.stepY <= this.minY) return;
-        this.currY -= this.stepY;
-        break;
-      case 'ArrowDown':
-        if (this.currY + this.stepY >= this.maxY) return;
-        this.currY += this.stepY;
-        break;
-      default:
-        break;
-    }
-    this.update(this.currX, this.currY);
-    if (this.currY === -6) {
-      infoHolder.innerText = `You won`;
-      currentScore += 1;
-      currentHolder.innerText = `Current score: ${currentScore}`;
-      setTimeout(() => {
-        player.update(this.startX, this.startY);
-        infoHolder.innerText = `Let's go!`;
-      }, 1000);
-      if (currentScore > highScore) {
-        highScore = currentScore;
-        localStorage.setItem('highScore', highScore);
-        highHolder.innerText = `High score: ${highScore}`;
-      }
-    }
-  }
-}
-
 const allEnemies = [
-  new Enemy(65, getRandomNumber(150, 300)),
-  new Enemy(150, getRandomNumber(150, 300)),
-  new Enemy(235, getRandomNumber(150, 300)),
+  new Enemy(env.tileHeight, getRandomNumber(env.minEnemySpeed, env.maxEnemySpeed), player),
+  new Enemy(env.tileHeight * 2, getRandomNumber(env.minEnemySpeed, env.maxEnemySpeed), player),
+  new Enemy(env.tileHeight * 3, getRandomNumber(env.minEnemySpeed, env.maxEnemySpeed), player),
 ];
-const player = new Player();
 
 document.addEventListener('keyup', function (e) {
   const allowedKeys = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'];
@@ -144,6 +156,8 @@ document.addEventListener('keyup', function (e) {
 });
 
 document.addEventListener('DOMContentLoaded', () => {
-  currentHolder.innerText = `Current score: ${currentScore}`;
-  highHolder.innerText = `High score: ${highScore}`;
+  env.updateScore(env.welcomeMessage);
+  console.log(player);
+  console.log(env);
+  allEnemies.forEach(enemy => console.log(enemy));
 });
