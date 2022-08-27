e966bbfddb91f245026b8574e192fa7de3a3661d
Bohdan Mukha <by.muha@gmail.com>
2022-08-27 20:12:13 +0300

    player movement works

--- js/app.js
+++ js/app.js
@@ -1,44 +1,60 @@
-const env = {
-  fieldMinX: 0,
-  fieldMinY: -10,
-  fieldMaxX: 505,
-  fieldMaxY: 425,
-  tileWidth: 101,
-  tileHeight: 85,
-  tilesCountX: 5,
-  tilesCountY: 6,
-  currentScore: 0,
-  isGameActive: true,
-  highScoreHolder: document.querySelector('.high'),
-  messageHolder: document.querySelector('.info'),
-  welcomeMessage: "Let's go!",
-  winMessage: 'You win!',
-  loseMessage: 'You lose!',
-  highScore: localStorage.getItem('highScore') ?? 0,
-  currentScoreHolder: document.querySelector('.current'),
-  updateGameInfo: function (message) {
+class Environment {
+  constructor() {
+    this.characterWidth = 101;
+    this.characterHeight = 83;
+    this.fieldMinX = 0;
+    this.fieldMinY = -10;
+    this.fieldMaxX = 505;
+    this.fieldMaxY = 405;
+    this.isGameOn = true;
+  }
+  startGame() {
+    this.isGameOn = true;
+  }
+  stopGame() {
+    this.isGameOn = false;
+  }
+}
+
+const env = new Environment();
+
+class ScoreBoard {
+  constructor(highScoreHolder, messageHolder, currentScoreHolder) {
+    this.currentScore = 0;
+    this.highScoreHolder = document.querySelector(highScoreHolder);
+    this.messageHolder = document.querySelector(messageHolder);
+    this.welcomeMessage = "Let's go!";
+    this.winMessage = 'You win!';
+    this.loseMessage = 'You lose!';
+    this.highScore = localStorage.getItem('highScore') ?? 0;
+    this.currentScoreHolder = document.querySelector(currentScoreHolder);
+  }
+  updateGameInfo(message) {
     this.currentScoreHolder.innerText = `Current score: ${this.currentScore}`;
     this.highScoreHolder.innerText = `High score: ${this.highScore}`;
     this.messageHolder.innerText = `${message}`;
-  },
-  incrementScore: function () {
+  }
+  incrementScore() {
     this.currentScore += 1;
     if (this.currentScore > this.highScore) {
       this.highScore = this.currentScore;
       this.saveHighScore(this.highScore);
     }
-  },
-  saveHighScore: function (score) {
+  }
+  saveHighScore(score) {
     localStorage.setItem('highScore', score);
-  },
-  resetScore: function (resetHighScore = false) {
+  }
+  resetScore(resetHighScore = false) {
     if (resetHighScore) {
       this.highScore = 0;
       this.saveHighScore();
     }
     this.currentScore = 0;
-  },
-};
+  }
+}
+
+const scoreBoard = new ScoreBoard('.high', '.message', '.current');
+console.log(scoreBoard);
 
 const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
@@ -47,25 +63,23 @@ const directions = ['left', 'right'];
 const getRandomDirection = () => directions[Math.floor(Math.random() * directions.length)];
 
 class Player {
-  constructor() {
+  constructor(env, scoreBoard) {
+    this.env = env;
+    this.scoreBoard = scoreBoard;
     this.sprite = 'images/char-boy.png';
-    this.startX = env.tileWidth * 2;
-    this.startY = env.fieldMinY + env.tileHeight * 5;
+    this.startX = this.env.characterWidth * 2;
+    this.startY = this.env.fieldMinY + this.env.characterHeight * 5;
     this.currX = this.startX;
     this.currY = this.startY;
     this.resetDelay = 1000;
   }
 
   update(newX = this.currX, newY = this.currY) {
-    if (!env.isGameActive) {
+    if (!this.env.isGameOn) {
       return;
     }
     (this.currX = newX), (this.currY = newY);
     // console.log(`x: ${this.currX}, y:${this.currY}`);
-  }
-
-  reset() {
-    this.update(this.startX, this.startY);
   }
 
   render() {
@@ -74,45 +88,46 @@ class Player {
   }
 
   handleInput(code) {
-    if (!env.isGameActive) {
+    if (!this.env.isGameOn) {
       return;
     }
     switch (code) {
       case 'ArrowLeft':
-        if (this.currX - env.tileWidth < env.fieldMinX) return;
-        this.currX -= env.tileWidth;
+        if (this.currX - this.env.characterWidth < this.env.fieldMinX) return;
+        this.currX -= this.env.characterWidth;
         break;
       case 'ArrowRight':
-        if (this.currX + env.tileWidth >= env.fieldMaxX) return;
-        this.currX += env.tileWidth;
+        if (this.currX + this.env.characterWidth >= this.env.fieldMaxX) return;
+        this.currX += this.env.characterWidth;
         break;
       case 'ArrowUp':
-        if (this.currY - env.tileHeight < env.fieldMinY) return;
-        this.currY -= env.tileHeight;
+        if (this.currY - this.env.characterHeight < this.env.fieldMinY) return;
+        this.currY -= this.env.characterHeight;
         break;
       case 'ArrowDown':
-        if (this.currY + env.tileHeight >= env.fieldMaxY) return;
-        this.currY += env.tileHeight;
+        if (this.currY + this.env.characterHeight >= this.env.fieldMaxY) return;
+        this.currY += this.env.characterHeight;
         break;
       default:
         break;
     }
+    console.log(`x: ${this.currX}, y:${this.currY}`);
     this.update(this.currX, this.currY);
-    if (this.currY === env.fieldMinY) {
-      env.isGameActive = false;
-      env.incrementScore();
-      env.updateGameInfo(env.winMessage);
+    if (this.currY === this.env.fieldMinY) {
+      this.env.isGameOn = false;
+      this.scoreBoard.incrementScore();
+      this.scoreBoard.updateGameInfo(this.scoreBoard.winMessage);
       setTimeout(() => {
-        env.isGameActive = true;
-        player.reset();
-        env.updateGameInfo(env.welcomeMessage);
+        this.env.isGameOn = true;
+        player.update(this.startX, this.startY);
+        this.scoreBoard.updateGameInfo(this.scoreBoard.welcomeMessage);
       }, this.resetDelay);
     }
-    console.log(`x: ${this.currX}, y:${this.currY}`);
+    // console.log(`x: ${this.currX}, y:${this.currY}`);
   }
 }
 
-const player = new Player();
+const player = new Player(env, scoreBoard);
 
 class Enemy {
   constructor(startY, player) {
@@ -137,21 +152,24 @@ class Enemy {
       this.player.currX >= this.currX &&
       this.player.currX <= this.currX + env.tileWidth &&
       this.player.currY >= this.currY &&
-      this.player.currY <= this.currY + env.tileHeight
+      this.player.currY <= this.currY + env.characterHeight
     ) {
+      // console.log(`playerX: ${this.player.currX}`);
+      // console.log(`playerY: ${this.player.currY}`);
+      // console.log(`enemyX: ${this.currX}`);
+      // console.log(`enemyY: ${this.currY}`);
       return true;
     }
     // console.log('no collision');
     return false;
   }
   update(dt) {
-    console.log(`x: ${this.currX}, y:${this.currY}`);
     if (this.checkCollision()) {
-      console.log('collision');
+      // console.log('collision');
       env.resetScore();
       env.updateGameInfo(env.loseMessage);
       setTimeout(() => {
-        this.player.updateGameInfo(player.startX, player.startY);
+        this.player.update(this.player.startX, this.player.startY);
         env.updateGameInfo(env.welcomeMessage);
       }, 1000);
       this.player.update(player.startX, player.startY);
@@ -165,7 +183,9 @@ class Enemy {
       this.direction = 'right';
       this.sprite = `images/enemy-bug-${this.direction}.png`;
     }
+    // dt = parseInt(dt * 100);
     this.currX = this.currX + dt * this.mult;
+    // console.log(`x: ${this.currX}, y:${this.currY} dt:${dt}`);
   }
   render() {
     // eslint-disable-next-line no-undef
@@ -173,11 +193,11 @@ class Enemy {
   }
 }
 
-const allEnemies = [
-  new Enemy(env.tileHeight, player),
-  new Enemy(env.tileHeight * 2, player),
-  new Enemy(env.tileHeight * 3, player),
-];
+const firstEnemyY = env.fieldMinY + env.characterHeight;
+const secondEnemyY = env.fieldMinY + env.characterHeight * 2;
+const thirdEnemyY = env.fieldMinY + env.characterHeight * 3;
+
+const allEnemies = [new Enemy(firstEnemyY, player), new Enemy(secondEnemyY, player), new Enemy(thirdEnemyY, player)];
 
 document.addEventListener('keyup', function (e) {
   const allowedKeys = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'];
@@ -188,12 +208,12 @@ const isItFriday13thToday = () => new Date().getDay() === 5 && new Date().getDat
 
 document.addEventListener('DOMContentLoaded', () => {
   if (isItFriday13thToday()) {
-    env.highScore = 0;
-    env.saveHighScore(env.highScore);
-    env.updateGameInfo(`It's Friday 13th! Your High Score is now gone!`);
+    scoreBoard.highScore = 0;
+    scoreBoard.saveHighScore(env.highScore);
+    scoreBoard.updateGameInfo(`It's Friday 13th! Your High Score is now gone!`);
   } else {
-    console.log(isItFriday13thToday());
-    env.updateGameInfo(env.welcomeMessage);
+    // console.log(isItFriday13thToday());
+    scoreBoard.updateGameInfo(scoreBoard.welcomeMessage);
     console.log(player);
     console.log(env);
     allEnemies.forEach(enemy => console.log(enemy));
